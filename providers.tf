454d97ac3ff13a6c24716174145ebaa1078db550
kirillKhr <kirill.hrustalev.2001@mail.ru>
2025-02-18 20:44:41 +0300

    add terraform providers, variables

---
+++ providers.tf
@@ -0,0 +1,20 @@
+terraform{
+    required_version = ">= 0.13.0"
+    required_providers{
+        openstack = {
+            source  = "terraform-provider-openstack/openstack"
+            version = "1.51.1"
+        }
+    }
+}
+ 
+provider "openstack"{
+    cloud = "openstack"
+}
+
+provider "openstack" {
+    auth_url    = var.openstack_auth_url
+    user_name   = var.openstack_username
+    password    = var.openstack_password
+    domain_name = var.openstack_domain_name
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "openstack_auth_url" {
+  description = "Openstack auth URL"
+}
+
+variable "openstack_username" {
+  description = "Openstack username"
+}
+
+variable "openstack_password" {
+  description = "Openstack password"
+}
+
+variable "openstack_domain_name" {
+  description = "Openstack domain name"
+  default     = "Default"
+}
