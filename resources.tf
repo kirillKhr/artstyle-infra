9a18b6c324e90f979277e96100ebdb38227f91d9
kirillKhr <kirill.hrustalev.2001@mail.ru>
2025-02-19 23:24:54 +0300

    add resources

---
+++ resources.tf
@@ -0,0 +1,20 @@
+resource "openstack_compute_instance_v2" "goryunov_infra_terraform_ansible" {
+    name        = var.instance_name
+    image_name  = var.image_name
+    flavor_name = var.flavor_name
+    key_pair    = var.key_name
+
+    block_device {
+        uuid                  = "253a6ce9-fd9d-4a14-bbac-097a6eb8fb10"
+        source_type           = "image"
+        destination_type      = "volume"
+        volume_size           = 20
+        delete_on_termination = true
+    }
+
+    network {
+        name = var.network_name
+    }
+
+    security_groups = var.security_groups
+}
--- variables.tf
+++ variables.tf
@@ -14,3 +14,32 @@ variable "openstack_domain_name" {
   description = "Openstack domain name"
   default     = "Default"
 }
+
+variable "instance_name" {
+  description = "Name of the instance"
+  default     = "khrustalyov_infra_terra_ansible"
+}
+
+variable "key_name" {
+  description = "Name of the ssh key"
+}
+
+variable "image_name" {
+  description = "Image to use"
+  default     = "ubuntu-20.04"
+}
+
+variable "flavor_name" {
+  description = "Flavor of the instance"
+  default     = "m1.small"
+}
+
+variable "network_name" {
+  description = "Network to attach"
+}
+
+variable "security_group" {
+  description = "Security groups to use"
+  type        = list(string)
+  default     = ["default"]
+}
